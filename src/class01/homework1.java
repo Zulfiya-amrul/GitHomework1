79a39546e395417caeaaaa56dbf876fca1aa790c
zulfiya-amrul <zulfiya.amrul@gmail.com>
2022-05-14 18:25:10 -0700

    first commit

---
+++ src/class01/homework1.java
@@ -0,0 +1,10 @@
+package class01;
+
+public class homework1 {
+    public static void main(String[] args) {
+        for (int i = 0; i <=100 ; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
