ff04d823216a64a4e93512dd14d9f40711b0d626
zulfiya-amrul <zulfiya.amrul@gmail.com>
2022-05-14 19:00:37 -0700

    clone commit

--- src/class01/homework1.java
+++ src/class01/homework1.java
@@ -2,7 +2,7 @@ package class01;
 
 public class homework1 {
     public static void main(String[] args) {
-        for (int i = 0; i <=100 ; i++) {
+        for (int i = 0; i <=50 ; i++) {
             System.out.println(i);
 
         }
